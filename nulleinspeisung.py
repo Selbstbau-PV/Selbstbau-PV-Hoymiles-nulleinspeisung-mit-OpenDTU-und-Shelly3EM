785ee154c07f8a437eeaf63d37a6c6bd093fc7fc
Selbstbau PV <info@selbstbau-pv.de>
2023-05-01 21:28:00 +0200

    Umstrukturierung, Meldungen (#4), Programmlogik (#5)

--- nulleinspeisung.py
+++ nulleinspeisung.py
@@ -1,75 +1,69 @@
 import requests, time
 from requests.auth import HTTPBasicAuth
 
-# Diese Daten müssen angepasst werden: zeile 5 - 12
-serial = "112100000000" # Seriennummern der Hoymiles Wechselrichter
-maximum_wr = 300 # Maximum ausgabe des wechselrichters
 
-dtuIP = '192.100.100.20' # IP Adresse von OpenDTU
-dtuNutzer = 'admin' # OpenDTU Nutzername
-dtuPasswort = 'openDTU42' # OpenDTU Passwort
+# Diese Daten müssen angepasst werden:
+serial = "112100000000" # Seriennummer des Hoymiles Wechselrichters
+maximum_wr = 300 # Maximale Ausgabe des Wechselrichters
+minimum_wr = 100 # Minimale Ausgabe des Wechselrichters
 
-shellyIP = '192.100.100.30' #IP Adresse von Shelly 3EM
+dtu_ip = '192.100.100.20' # IP Adresse von OpenDTU
+dtu_nutzer = 'admin' # OpenDTU Nutzername
+dtu_passwort = 'openDTU42' # OpenDTU Passwort
+
+shelly_ip = '192.100.100.30' # IP Adresse von Shelly 3EM
 
 
 while True:
     try:
         # Nimmt Daten von der openDTU Rest-API und übersetzt sie in ein json-Format
-        r = requests.get(url = f'http://{dtuIP}/api/livedata/status/inverters' ).json()
+        r = requests.get(url = f'http://{dtu_ip}/api/livedata/status/inverters' ).json()
 
         # Selektiert spezifische Daten aus der json response
-        reachable   = r['inverters'][0]['reachable'] # ist DTU erreichbar ?
-        producing   = int(r['inverters'][0]['producing']) # produziert der Wechselrichter etwas ?
-        altes_limit = int(r['inverters'][0]['limit_absolute']) # wo war das alte Limit gesetzt
+        reachable   = r['inverters'][0]['reachable'] # Ist DTU erreichbar?
+        producing   = int(r['inverters'][0]['producing']) # Produziert der Wechselrichter etwas?
+        altes_limit = int(r['inverters'][0]['limit_absolute']) # Altes Limit
         power_dc    = r['inverters'][0]['AC']['0']['Power DC']['v']  # Lieferung DC vom Panel
         power       = r['inverters'][0]['AC']['0']['Power']['v'] # Abgabe BKW AC in Watt
-
-        # Nimmt Daten von der Shelly 3EM Rest-API und übersetzt sie in ein json-Format
-        phaseA      = requests.get(f'http://{shellyIP}/emeter/0', headers={"Content-Type": "application/json"}).json()['power']
-        phaseB      = requests.get(f'http://{shellyIP}/emeter/1', headers={"Content-Type": "application/json"}).json()['power']
-        phaseC      = requests.get(f'http://{shellyIP}/emeter/2', headers={"Content-Type": "application/json"}).json()['power']
-        grid_sum    = phaseA + phaseB + phaseC # Aktueller Bezug im Chalet - rechnet alle Phasen zusammen
-        setpoint    = 0     # Neues Limit in Watt
     except:
-        print("Ein Fehler ist bei der Datenverarbeitung aufgetreten")
-        print("Vorhergehenden Werte werden verwendet")
-        print("Reseting.....")
-
-    # Setzt ein limit auf das Wechselrichter
-    def setLimit(Serial, Limit):
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        payload = f'''data={{"serial":"{Serial}", "limit_type":0, "limit_value":{Limit}}}'''
-        try:
-            newLimit = requests.post(url=f'http://{dtuIP}/api/limit/config', data=payload, auth=HTTPBasicAuth(dtuNutzer, dtuPasswort), headers=headers)
-            print('Konfiguration Stauts:', newLimit.json()['type'])
-        except:
-            print("Konfiguration's fehler ... Konfiguration konnte nicht gesendet werden")
+        print('Fehler beim Abrufen der Daten von openDTU')
+    try:
+        # Nimmt Daten von der Shelly 3EM Rest-API und übersetzt sie in ein json-Format
+        phase_a     = requests.get(f'http://{shelly_ip}/emeter/0', headers={'Content-Type': 'application/json'}).json()['power']
+        phase_b     = requests.get(f'http://{shelly_ip}/emeter/1', headers={'Content-Type': 'application/json'}).json()['power']
+        phase_c     = requests.get(f'http://{shelly_ip}/emeter/2', headers={'Content-Type': 'application/json'}).json()['power']
+        grid_sum    = phase_a + phase_b + phase_c # Aktueller Bezug - rechnet alle Phasen zusammen
+    except:
+        print('Fehler beim Abrufen der Daten von Shelly 3EM')
 
     # Werte setzen
-    print("aktueller Bezug - Haus:   ",grid_sum)
+    print(f'\nBezug: {round(grid_sum, 1)} W, Produktion: {round(power, 1)} W, Verbrauch: {round(grid_sum + power, 1)} W')
     if reachable:
-        # Setzen Sie den Grenzwert auf den höchsten Wert, wenn er über dem zulässigen Höchstwert liegt.
-        if (altes_limit >= maximum_wr or grid_sum >= maximum_wr or setpoint >= maximum_wr):
-            print("setze Limiter auf maximum")
+        setpoint = grid_sum + altes_limit - 5 # Neues Limit in Watt
+
+        # Fange oberes Limit ab
+        if setpoint > maximum_wr:
             setpoint = maximum_wr
+            print(f'Setpoint auf Maximum: {maximum_wr} W')
+        # Fange unteres Limit ab
+        elif setpoint < minimum_wr:
+            setpoint = minimum_wr
+            print(f'Setpoint auf Minimum: {minimum_wr} W')
+        else:
+            print(f'Setpoint berechnet: {round(grid_sum, 1)} W + {round(altes_limit, 1)} W - 5 W = {round(setpoint, 1)} W')
 
-        # wir weniger bezogen als maximum_wr dann neues Limit ausrechnen
-        if (grid_sum+altes_limit) <= maximum_wr:
-            setpoint = grid_sum + altes_limit - 5
-            print("setpoint:",grid_sum,"+",altes_limit,"-5 ")
-            print("neues Limit berechnet auf ",setpoint)
-        if setpoint <= 100:
-            setpoint = 100
-            print("setpoint: 100 minimum gesetzt")
-            print("neues Limit festgelegt auf ",setpoint)
+        if setpoint != altes_limit:
+            print(f'Setze Inverterlimit von {round(altes_limit, 1)} W auf {round(setpoint, 1)} W... ', end='')
+            # Neues Limit setzen
+            try:
+                r = requests.post(
+                    url = f'http://{dtu_ip}/api/limit/config',
+                    data = f'data={{"serial":"{serial}", "limit_type":0, "limit_value":{setpoint}}}',
+                    auth = HTTPBasicAuth(dtu_nutzer, dtu_passwort),
+                    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+                )
+                print(f'Konfiguration gesendet ({r.json()["type"]})')
+            except:
+                print('Fehler beim Senden der Konfiguration')
 
-        print("setze Einspeiselimit auf: ",setpoint)
-        # neues limit setzen
-        setLimit(serial, setpoint)
-        print("Solarzellenstrom:",power,"  Setpoint:",setpoint)
-
-        time.sleep(5) # wait
-
-    # Wenn der Wechselrichter nicht erreicht werden kann, wird der limit auf maximum gestellt
-    if setpoint == 0: setpoint = grid_sum
-    if not reachable: setpoint = maximum_wr
+    time.sleep(5) # wait
